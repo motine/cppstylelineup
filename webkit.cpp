f30b8b9d03c03c75d59093429910c1944d0cad31
Sasha Kozlov <aleks.a.kozlov@gmail.com>
2019-12-09 17:32:40 +0100

    webkit: Fix a getter name
    According to the WebKit coding style, a simple getter should not contain
    a "get" prefix.
    
    Signed-off-by: Sasha Kozlov <aleks.a.kozlov@gmail.com>

--- webkit.cpp
+++ webkit.cpp
@@ -14,7 +14,7 @@ public:
     ~Cowboy();
 
     void shoot(std::string who);
-    int getAge() { return m_age; } // minor functions have lower case
+    int age() { return m_age; } // minor functions have lower case
 
 protected:
     void makeBang(const int& howMany);
